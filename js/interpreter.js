b54d2cecae3dc79898157ffe2eb05327c8dd2b04
jinkool <jinkyulim96@gmail.com>
2019-11-18 21:03:21 +1300

    added real time display on the editor

--- js/interpreter.js
+++ js/interpreter.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   });
 
 
-  var mark = editor.markText({line: 6, ch: 1}, {line: 6, ch: 10}, {css: "background-color : red"});
+  var mark = editor.markText({line: 0, ch: 0}, {line: 0, ch: 0});
 
   const CMD_MEMORY_INC = "WOW"
   const CMD_MEMORY_DEC = "wow"
@@ -33,6 +33,10 @@ $(document).ready(function () {
   });
 
   async function interpret(s) {
+    var lines = s.split(/\r?\n/);
+    var lineLengths = [];
+    lines.forEach(line => lineLengths.push(line.length + 1));
+
     editor.setOption("readOnly", "nocursor");
     $("#memory-container").html("<div id=\"line0\">"+ "00000000 00 00 00 00 00 00 00 00 ........</div><div id=\"line1\">"+ "00000008 00 00 00 00 00 00 00 00 ........</div><div id=\"line2\">"+ "00000016 00 00 00 00 00 00 00 00 ........</div>");
     var mem = [0];
@@ -47,10 +51,10 @@ $(document).ready(function () {
       await sleep($("#" + HTML_DELAY_ID).val() * 10);
       i += 3;
       switch (s.substring(i - 3, i)) {
-
         case CMD_MEMORY_DEC:
           mem[mem_pos] = (((mem[mem_pos]-1)%MEMORY_SIZE_LIMIT)+MEMORY_SIZE_LIMIT)%MEMORY_SIZE_LIMIT;
           processLine(mem, mem_pos);
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_REG_INC:
@@ -61,25 +65,30 @@ $(document).ready(function () {
           if (parseInt($('#' + HTML_MEMORY_ID).children().last().attr('id').substring(4)) < Math.floor(mem_pos / 8)) {
             $("#" + HTML_MEMORY_ID).append("<div id=\"line" + Math.floor(mem_pos / 8) + "\">"+  (Math.floor(mem_pos / 8) * 8).toString().padStart(8,0) +" 00 00 00 00 00 00 00 00 ........</div>");
           }
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_INPUT:
           mem[mem_pos] = ((parseInt(input[current_input_pos++])%MEMORY_SIZE_LIMIT)+MEMORY_SIZE_LIMIT)%MEMORY_SIZE_LIMIT;
           processLine(mem, mem_pos);
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_LOOP_CLOSE:
           if (mem[mem_pos] != 0) {
             i = brac_open_pos[brac_open_pos.length - 1];
           }
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_REG_DEC:
           mem_pos--;
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_OUTPUT:
           document.getElementById(HTML_OUTPUT_ID).value += String.fromCharCode(mem[mem_pos]);
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_LOOP_OPEN:
@@ -88,20 +97,24 @@ $(document).ready(function () {
             i = i + 1 + s.substring(i + 1).indexof(CMD_LOOP_CLOSE);
             brac_open_pos.pop();
           }
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_MEMORY_INC:
           mem[mem_pos] = (((mem[mem_pos]+1)%MEMORY_SIZE_LIMIT)+MEMORY_SIZE_LIMIT)%MEMORY_SIZE_LIMIT;
           processLine(mem, mem_pos);
+          highlightElement(i-3,lineLengths,3);
           break;
 
         case CMD_RESET:
           mem[mem_pos] = 0;
+          highlightElement(i-3,lineLengths,3);
           break;
         case "woo":
           if (s[i] == "w") {
             reg = mem[mem_pos];
             i++;
+            highlightElement(i-3,lineLengths,4);
           }
           else {
             i -= 2;
@@ -111,6 +124,7 @@ $(document).ready(function () {
           if (s[i] == "W") {
             mem[mem_pos] += reg;
             i++;
+            highlightElement(i-3,lineLengths,4);
           }
           else {
             i -= 2;
@@ -156,7 +170,23 @@ $(document).ready(function () {
     $("#line" + Math.floor(mem_pos / 8)).html(output);
   }
 
-  function highlightElement(pos, input){
+  function highlightElement(pos, lineLengths, length){
+    mark.clear();
+
+    var currLine = 0;
+
+    while (true){
+      if (pos>lineLengths[currLine]){
+        pos-=lineLengths[currLine++];
+      }
+      else{
+        //deal with line breaks
+        mark = editor.markText({line: currLine, ch: pos}, {line: currLine, ch: pos + length}, {css: "background-color : red"});
+        break;
+      }
+    }
+
+
 
   }
 
